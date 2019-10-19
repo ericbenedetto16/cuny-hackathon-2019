835f9dddf42de3862ab38e3288508c8afe81102f
Vin Ndokaj <vincentndokaj@Vincents-MacBook-Air.local>
2019-10-19 19:38:32 -0400

    completed option inteface... finally

--- client/screens/OptionScreen.js
+++ client/screens/OptionScreen.js
@@ -1,45 +1,73 @@
 import React, { useEffect } from 'react';
 import { 
-    Text,
     TouchableOpacity, 
     Image,
+    ImageBackground,
+    View,
     StyleSheet,
-    View
+    SafeAreaView,
+    Dimensions
 } from 'react-native'
+import newLinking from 'expo/build/Linking/Linking';
 
 export const OptionScreen = (props) => {
 
     const styles = StyleSheet.create({
-        icon:{
+        container:{
             flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
+            marginTop: '95%',
+        },
+        image:{
+           width: "100%",
+           height: "100%",
+           padding: 60,
+        },
+        prompt:{
+           width: "100%",
+           height: "100%",
+           padding: 35,
+           resizeMode: 'stretch',
         }
+
     })
 
     //const {navigate} = props.navigation;
 
-    return(
-        <View style= {styles.icon} >
-            <TouchableOpacity
-                key={'water'}
-                //onPress={() => navigate('Map', {type: 'water'})}
-                >
-                <Image
-                    style = {styles.icon}
-                    source={require('../assets/RB.jpg')}
-                />
-            </TouchableOpacity>
+    return(     
+        <View style={{flex:1,}}>
+            <ImageBackground 
+                    source={require('../assets/background.png')}
+                    imageStyle={{resizeMode:'stretch'}}
+                    style={{flex:1,}}
+                    >                    
+                    <SafeAreaView style= {styles.container}>
 
-            <TouchableOpacity
-                key={'restroom'}
-                //onPress={() => navigate('Map', {type: 'restroom'})}
-                >
-                <Image
-                    style = {styles.icon}
-                    source={require('../assets/RB.jpg')}
-                />
-            </TouchableOpacity>
+                        <Image 
+                            source={require('../assets/prompt.png')}
+                            style={styles.prompt}
+                            />
+
+                        <TouchableOpacity
+                            key={'water'}
+                            //onPress={() => navigate('Map', {type: 'water'})}
+                            >
+                            <Image
+                                style = {styles.image}
+                                source={require('../assets/WaterButton.png')}
+                            />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            key={'restroom'}
+                            //onPress={() => navigate('Map', {type: 'restroom'})}
+                            >
+                            <Image
+                                style = {styles.image}
+                                source={require('../assets/RestroomButton.png')}
+                            />
+                        </TouchableOpacity>
+                    </SafeAreaView>
+            </ImageBackground>
         </View>
     )
 }
